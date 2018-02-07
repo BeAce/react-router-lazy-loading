bb47c091ce6adaaf415c463eb11f4d3505dbe02c
BeAce <beaceshimin@gmail.com>
2018-02-07 18:28:12 +0800

    feat(containers): the containers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Router, Route, Switch, Link } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+import asyncComponent from './AsyncComponent';
+import "./App.css";
+const Home = asyncComponent(() => import('./containers/Home').then(module => module.default));
+const Maps = asyncComponent(() => import('./containers/Maps').then(module => module.default));
+const Blog = asyncComponent(() => import('./containers/Blog').then(module => module.default));
+const NotFound = asyncComponent(() => import('./containers/NotFound').then(module => module.default));
+
+const history = createBrowserHistory();
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router history={history}>
+        <div>
+          <header className="header">
+            <nav className="navbar container">
+              <div className="navbar-brand">
+                <Link to="/">
+                  <span className="navbar-item">Lazy Loading Routes</span>
+                </Link>
+              </div>
+
+              <div className="navbar-end">
+                <Link to="/maps">
+                  <span className="navbar-item">Maps</span>
+                </Link>
+                <Link to="/blog">
+                  <span className="navbar-item">Blog</span>
+                </Link>
+              </div>
+            </nav>
+          </header>
+          <section className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/maps" component={Maps} />
+              <Route path="/blog" component={Blog} />
+              <Route path="*" component={NotFound} />
+            </Switch>
+          </section>
+        </div>
+      </Router>
     );
   }
 }
